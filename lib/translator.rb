5732068c32bca2346da5c774223d8b7aa7796c67
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-11 04:45:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,8 @@
 require "yaml"
 
 def load_library(path)
-  #set a hash with get_meaning, get_emoticon as keys, empty hash for value
   emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
-  #load the YAML file. It has meaning(angel, angry,..) as key and the symbol as value(describe)
   YAML.load_file(path).each do |meaning, describe|
-     #set eng, jan from describe, so eng would be the first value, jan the second
      eng, jan = describe
      emoticons["get_meaning"][jan] = meaning
      emoticons["get_emoticon"][eng] = jan
@@ -15,7 +12,7 @@ end
 
 
   def get_japanese_emoticon(path, emoticon)
-  emoticons = load_library(path) #call load_library
+  emoticons = load_library(path) 
   result = emoticons["get_emoticon"][emoticon]
   if result
     result
